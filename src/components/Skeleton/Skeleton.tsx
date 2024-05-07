114ad82b5be4da39c880e48728bab3ac398c7dd9
Petrochenk0 <96386665+Petrochenk0@users.noreply.github.com>
2024-05-07 10:55:38 +0300

    add skeleton

--- src/components/Skeleton/Skeleton.tsx
+++ src/components/Skeleton/Skeleton.tsx
@@ -1,3 +1,5 @@
+import styles from './styles.module.css';
+
 type SkeletonProps = {
   count: number;
   type: 'banner' | 'item';
@@ -6,12 +8,19 @@ type SkeletonProps = {
 export default function Skeleton({ count = 1, type }: SkeletonProps) {
   console.log(count, type);
 
-  // return <>
-  // {count > 1 ? (<ul>
-  //   {[Array(count)].map((_, index)=>{
-  //     return <li key={index}>
-  //       <SkeletonItem type={type} />
-  //     </li>
-  //   })}
-  // </ul>)}</>;
+  return (
+    <div>
+      {count > 1 ? (
+        <ul>
+          {[Array(count)].map((_, index) => {
+            return (
+              <li key={index} className={type === 'banner' ? styles.banner : styles.item}></li>
+            );
+          })}
+        </ul>
+      ) : (
+        <li className={type === 'banner' ? styles.banner : styles.item}></li>
+      )}
+    </div>
+  );
 }
--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -9,6 +9,7 @@ import styles from './styles.module.css';
 import { getNews } from '../../api/apiNews';
 export default function Main() {
   const [news, setNews] = React.useState([]);
+  const [loading, setLoading] = React.useState<boolean>(false);
   React.useEffect(() => {
     const fetchNews = async () => {
       try {
